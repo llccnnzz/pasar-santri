8833be89fb363b13bf9e845b52ab5b14301477d1
ijlik <ijlik.k5s@gmail.com>
2025-08-20 06:09:42 +0700

    fix code structure

--- config/media-library.php
+++ config/media-library.php
@@ -122,41 +122,41 @@ return [
      */
     'image_optimizers' => [
         Spatie\ImageOptimizer\Optimizers\Jpegoptim::class => [
-            '-m85',              // set maximum quality to 85%
-            '--force',           // ensure that progressive generation is always done also if a little bigger
-            '--strip-all',       // this strips out all text information such as comments and EXIF data
+            '-m85', // set maximum quality to 85%
+            '--force', // ensure that progressive generation is always done also if a little bigger
+            '--strip-all', // this strips out all text information such as comments and EXIF data
             '--all-progressive', // this will make sure the resulting image is a progressive one
         ],
         Spatie\ImageOptimizer\Optimizers\Pngquant::class => [
             '--force', // required parameter for this package
         ],
         Spatie\ImageOptimizer\Optimizers\Optipng::class => [
-            '-i0',    // this will result in a non-interlaced, progressive scanned image
-            '-o2',    // this set the optimization level to two (multiple IDAT compression trials)
+            '-i0', // this will result in a non-interlaced, progressive scanned image
+            '-o2', // this set the optimization level to two (multiple IDAT compression trials)
             '-quiet', // required parameter for this package
         ],
         Spatie\ImageOptimizer\Optimizers\Svgo::class => [
             '--disable=cleanupIDs', // disabling because it is known to cause troubles
         ],
         Spatie\ImageOptimizer\Optimizers\Gifsicle::class => [
-            '-b',  // required parameter for this package
+            '-b', // required parameter for this package
             '-O3', // this produces the slowest but best results
         ],
         Spatie\ImageOptimizer\Optimizers\Cwebp::class => [
-            '-m 6',     // for the slowest compression method in order to get the best compression.
+            '-m 6', // for the slowest compression method in order to get the best compression.
             '-pass 10', // for maximizing the amount of analysis pass.
-            '-mt',      // multithreading for some speed improvements.
-            '-q 90',    // quality factor that brings the least noticeable changes.
+            '-mt', // multithreading for some speed improvements.
+            '-q 90', // quality factor that brings the least noticeable changes.
         ],
         Spatie\ImageOptimizer\Optimizers\Avifenc::class => [
             '-a cq-level=23', // constant quality level, lower values mean better quality and greater file size (0-63).
-            '-j all',         // number of jobs (worker threads, "all" uses all available cores).
-            '--min 0',        // min quantizer for color (0-63).
-            '--max 63',       // max quantizer for color (0-63).
-            '--minalpha 0',   // min quantizer for alpha (0-63).
-            '--maxalpha 63',  // max quantizer for alpha (0-63).
+            '-j all', // number of jobs (worker threads, "all" uses all available cores).
+            '--min 0', // min quantizer for color (0-63).
+            '--max 63', // max quantizer for color (0-63).
+            '--minalpha 0', // min quantizer for alpha (0-63).
+            '--maxalpha 63', // max quantizer for alpha (0-63).
             '-a end-usage=q', // rate control mode set to Constant Quality mode.
-            '-a tune=ssim',   // SSIM as tune the encoder for distortion metric.
+            '-a tune=ssim', // SSIM as tune the encoder for distortion metric.
         ],
     ],
 
