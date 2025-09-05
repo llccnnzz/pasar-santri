eafdf206f26c229c0849b246321a48c707e0936e
Adi Nugroho <ijlik.k5s@gmail.com>
2025-09-05 20:25:26 +0700

    update halaman checkout

--- resources/views/buyer/checkout/success.blade.php
+++ resources/views/buyer/checkout/success.blade.php
@@ -6,13 +6,16 @@
 @section('content')
 <main class="main">
     <div class="container text-center my-5">
-        <h2 class="mb-3">🎉 Pesanan Berhasil Dibuat!</h2>
+        <h5 class="text-muted">🎉 Pesanan Berhasil Dibuat!</h5>
         <h5 class="text-muted mb-4">Terima kasih sudah berbelanja di toko kami.</h5>
 
-        <p class="text-muted">
-            Silahkan melanjutkan pembayaran pada aplikasi <strong>emaal</strong>.<br>
+        <h2 class="mb-3">
+            Silahkan melanjutkan pembayaran pada aplikasi
+        </h2>
+            <img src="/assets/imgs/theme/emaal.png" style="height: 40px; width: auto">
+        <h3 class="mb-3"><br>
             Berikut kode billing Anda:
-        </p>
+        </h3>
 
         @if($payments->count())
             <ul class="list-unstyled mt-3">
@@ -22,9 +25,23 @@
                             {{ $payment->reference_id }}
                         </span>
                     </li>
+                    <li>
+                        <span class="d-block text-muted display-6">
+                            Total Pembayaran
+                        </span>
+                    </li>
+                    <li>
+                        <span class="d-block fw-bold text-black-50 display-6">
+                            Rp. {{ number_format($payment->total_amount, 0) }}
+                        </span>
+                    </li>
                 @endforeach
             </ul>
         @endif
+
+        <p class="text-muted"><br>
+            Jika anda sudah melakukan pembayaran, silahkan konfirmasi pembayaran pada halaman akun saya.
+        </p>
 
         <a href="{{ route('account') }}" class="btn btn-primary mt-4">
             Lihat Pesanan Saya
