cc73e624d0a7fdca6ab85ada4ba3679ad6f26ed5
Adi Nugroho <ijlik.k5s@gmail.com>
2025-09-06 09:44:49 +0700

    Admin Route Structure

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'has.shop' => \App\Http\Middleware\HasShop::class,
             'has.approved.kyc' => \App\Http\Middleware\HasApprovedKyc::class,
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
